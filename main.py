82789177c0a86632b0d83065e27bf710eb384cfb
unknown <elkanouni.samir2019@gmail.com>
2023-04-24 22:26:11 +0100

    first try

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ RPM_Pin = PWM(Pin(8))
 RPM_Pin.duty_u16(32767)
 
 def update_RPM:
-    if(RPM_val > 7 && RPM_val < 10000): 
+    if(RPM_val > 10 && RPM_val < 10000): 
         RPM_Pin.freq(K*RPM_val)
 tim = Timer(period=1000, mode=Timer.ONE_SHOT, callback=update_RPM)
 tim.init(period=1000, mode=Timer.PERIODIC, callback=update_RPM)
