368fbc5a1ea9f02254e5bb6811e7be8d8e8f75d0
Mimouni-yasser <mimouniyas2@gmail.com>
2023-04-24 19:52:55 +0000

    added basic update RPM function

---
+++ main.py
@@ -0,0 +1,11 @@
+from machine import *
+
+
+RPM_Pin = PWM(Pin(8))
+RPM_Pin.duty_u16(32767)
+
+def update_RPM:
+    if(RPM_val > 7 && RPM_val < 10000): 
+        RPM_Pin.freq(K*RPM_val)
+tim = Timer(period=1000, mode=Timer.ONE_SHOT, callback=update_RPM)
+tim.init(period=1000, mode=Timer.PERIODIC, callback=update_RPM)
