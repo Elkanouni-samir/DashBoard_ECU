e17810051ad803f4b31a5057e9347eb12a8241c9
Elkanouni-samir <elkanouni.samir2019@gmail.com>
2023-05-01 19:46:28 +0100

    checked the codes of the MCP2515

--- CAN_Config.py
+++ CAN_Config.py
@@ -9,6 +9,7 @@ INSTRUCTION_READ = 0x03
 REGISTER_CANCTRL = 0x0F
 REGISTER_CANSTAT = 0x0E
 
+#The MCP2515 supports Mode 0,0 and Mode 1,1.
 spi = SPI(0, baudrate=10_000_000, polarity=0, phase=0, bits=8, sck=Pin(2), mosi=Pin(3), miso=Pin(4))
 cs = Pin(5, Pin.OUT) # Chip select for the MCP2515 module
 p2 = Pin(2, Pin.IN) # Interrupt coming from MCP2515 module
@@ -25,7 +26,7 @@ def initialize():
 
     # Configure the MCP2515 module
     write_register(REGISTER_CANCTRL, 0x80) # Set configuration mode
-    write_register(0x2A, 0x07) # Enable interrupt pin
+    write_register(0x2A, 0x07) # Configure the baud rate to 500 kbps?
     write_register(0x2B, 0x0F) # Enable interrupt on RX0 and RX1
 
     # Check the module status
@@ -48,13 +49,14 @@ def write_register(address, value):
 def irq_handler(pin):
     print("Interrupt from MCP2515 module")
     # Read the interrupt flags
-    flags = read_register(0x2C)
+    flags = read_register(0x2C) # Read CANINTF
     print("Interrupt flags: ", flags)
 
     # Read the data from RXB0 buffer
-    write_register(0x60, 0x00) # Set RXB0CTRL to read mode
+    write_register(0x60, 0x00) # Set RXB0CTRL to read mode using mask and filters (to turn off filters select mode 0x11 in the second argument)
     cs.value(1)
-    spi.write(bytes([0x90, 0x00])) # Read data from RXB0 buffer
+    #spi.write(bytes([0x90, 0x00])) # Read data from RXB0 buffer (start to read from RXB0SIDH)
+    spi.write(bytes([0x92, 0x00])) # Read data from RXB0 buffer (start to read from RXB0D0)
     data = spi.read(5) # Read 5 bytes of data
     cs.value(0)
     print("Data received from MCP2515 module: ", data)
